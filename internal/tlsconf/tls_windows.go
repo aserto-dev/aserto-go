8447d957b8854afcd0f7e5449269ba8dc813aceb
florindragos <florin@aserto.com>
2022-06-06 12:14:38 +0000

    Use SystemCertPool only when caCertPath is empty

--- internal/tlsconf/tls_windows.go
+++ internal/tlsconf/tls_windows.go
@@ -2,15 +2,34 @@ package tlsconf
 
 import (
 	"crypto/tls"
+	"crypto/x509"
+	"os"
+
+	"github.com/pkg/errors"
 )
 
-func TLSConfig(insecure bool) (*tls.Config, error) {
+func TLSConfig(insecure bool, caCertPath string) (*tls.Config, error) {
 	var tlsConf tls.Config
 
 	if insecure {
 		tlsConf.InsecureSkipVerify = true
-	} else {
-		tlsConf.MinVersion = tls.VersionTLS12
+		return &tlsConf, nil
 	}
+
+	if caCertPath != "" {
+		certPool := x509.NewCertPool()
+		caCertBytes, err := os.ReadFile(caCertPath)
+		if err != nil {
+			return nil, errors.Wrapf(err, "failed to read ca cert [%s]", caCertPath)
+		}
+
+		if !certPool.AppendCertsFromPEM(caCertBytes) {
+			return nil, errors.Wrapf(err, "failed to append client ca cert [%s]", caCertPath)
+		}
+		tlsConf.RootCAs = certPool
+	}
+
+	tlsConf.MinVersion = tls.VersionTLS12
+
 	return &tlsConf, nil
 }
